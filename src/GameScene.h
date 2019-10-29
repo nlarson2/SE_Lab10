5aa5f4d190c6bc6285bf18a93d9c20aa56f1bb9a
Nickolas Larson <nickolas.larson1991@gmail.com>
2019-10-28 23:43:41 -0700

    Lab 10

---
+++ src/GameScene.h
@@ -0,0 +1,50 @@
+
+/*
+Created By: Nickolas Larson
+Date: 10/28/2019
+
+Modified By:
+Modified Date:
+
+
+*/
+
+#ifndef GAMESCENE_H
+#define GAMESCENE_H
+
+
+#include <X11/keysym.h>
+#include "Image.h"
+#include "fonts.h"
+#include <GL/gl.h>
+/*
+#include "Enemy.h"
+#include "Winner.h"
+#include "GameOver.h"
+#include "Player.h"
+*/
+#include <stdio.h>
+
+struct Box {
+    Image * img;
+    float center[2];
+    float w;
+    float h;
+    //[0-1]
+    float rgb[3];
+};
+
+class GameScene{
+    protected:
+        int xres, yres;
+    public:
+        virtual ~GameScene(){}
+        virtual void Draw() = 0;
+        virtual GameScene * CheckMouse(int x, int y) = 0;
+        virtual GameScene * CheckKey(int key) = 0;
+        virtual GameScene * Update() = 0;
+ };
+
+
+
+#endif
---
+++ src/Image.h
@@ -0,0 +1,28 @@
+
+//Author: Gordon Griesel
+
+//Modified by: Nickolas Larson
+
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+
+class Image {
+	public:
+		int width, height;
+		unsigned char *data;
+		Image() {}
+		~Image();
+		Image(const char *fname);
+		void GenerateGLTexture(GLuint & texture);
+		static void GenerateGLTextureALPHA(GLuint & texture, const char * img);
+
+		
+};
+
+
+
+#endif
